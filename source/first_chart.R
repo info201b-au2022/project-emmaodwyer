04e2014b193850bf26da94020ef1b1750bbf42da
Jennifer <jngomez@uw.edu>
2022-11-13 18:20:24 -0800

    pushing chart 1 and 2

--- source/first_chart.R
+++ source/first_chart.R
@@ -1,6 +1,42 @@
-#source file that creates first chart
+# source file that creates first chart
 
-#getting data for chart 1
-library(dplyr)
+# loading packages 
+library("tidyverse")
+library(ggplot2)
+library(readr)
 
-prop_of_wildfires <- 
+# get data for chart 1
+US_AQI <- read_csv("Downloads/US_AQI.csv")
+View(US_AQI)
+
+# filter 'US_AQI' to the beginning of 1992 
+US_AQI <- US_AQI %>%
+  filter(Date > "1991-12-31")
+
+# create year column
+US_AQI <- US_AQI %>%
+  mutate(Year = format(as.Date(Date, format="%Y-%m-%d"),"%Y"))
+
+# extract year
+year <- table(US_AQI$Year)
+View(year)
+
+#making year into dataframe
+year_df <- as.data.frame(year)
+View(year_df)
+
+# find mean AQI by year
+US_AQI_mean <- US_AQI %>%
+  group_by(Year) %>%
+  summarise_at(vars(AQI), list(mean_aqi_per_year = mean))
+
+# gathering data
+data <- data.frame("num" = year_df$Freq, "year" = US_AQI_mean$Year)
+  View(data)
+  
+# bar chart of mean AQI by year 
+bar_chart <- 
+  ggplot(data, aes(x=year, y=num))+
+  geom_bar(width = 1, stat = "identity") 
+bar_chart
+
--- source/second_chart.R
+++ source/second_chart.R
@@ -1,1 +1,28 @@
-#source file that creates second chart
+# source file that creates second chart
+
+# loading packages 
+library("tidyverse")
+library(ggplot2)
+library(data.table)
+
+# loading data 
+wildfires <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-emmaodwyer/main/data/FW_Veg_Rem_Combined.csv")
+View(wildfires)
+
+# finding number of duplicated causes 
+causes <- table(wildfires$stat_cause_descr)
+View(causes)
+
+# making 'causes' a dataframe 
+causes_df <- as.data.frame(causes)
+causes_df <- causes_df %>%
+  select("cause" = Var1, "num_of_causes" = Freq)
+View(causes_df)
+
+# proportion of causes of wildfires pie chart 
+pie <-  
+  ggplot(causes_df, aes(x="", y=num_of_causes, fill=cause))+
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0)
+pie
+
