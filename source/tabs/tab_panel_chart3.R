05345af3f610ab3b272f815cb5440adf09553794
emmaodwyer <s2105757@ed.ac.uk>
2022-12-06 15:31:09 -0800

    Progress on Chart 3 - not complete yet

--- source/tabs/tab_panel_chart3.R
+++ source/tabs/tab_panel_chart3.R
@@ -2,7 +2,34 @@
 
 library(shiny)
 
+# loading data set
+US_AQI <- list.files(path = "../data/aqi_data",
+                     pattern = "*.csv", full.names = TRUE) %>%
+  lapply(read_csv) %>%
+  bind_rows 
+
+
+states <- unique(US_AQI$state_name)
+states <- append(states, "United States")
+states <- sort(states)
+
+View(states)
 tab_panel_chart3 <-tabPanel(
-    "Chart 3",
-    p("This is chart 3.")
+    "The Number of Wildfires in the U.S and the Mean AQI per Year",
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(
+          inputId ="state", label = h4("State"), choices = states, selected = "United States"
+        ),
+        h4("Average Air Quality Index and Number of Wildfires per Year"),
+        
+        p("This is the description of chart 3.")
+     
+         ),
+        
+        mainPanel(
+          plotlyOutput(OutputId = "chart3")
+      )
+    )
+    
 )
