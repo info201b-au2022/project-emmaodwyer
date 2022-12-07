e7495788dabc2a3cd39ee5957d3b559aca6b0ea9
emmaodwyer <s2105757@ed.ac.uk>
2022-12-06 16:21:25 -0800

    Tab for chart 3 complete

--- source/tabs/tab_panel_chart3.R
+++ source/tabs/tab_panel_chart3.R
@@ -13,7 +13,7 @@ states <- unique(US_AQI$state_name)
 states <- append(states, "United States")
 states <- sort(states)
 
-View(states)
+
 tab_panel_chart3 <-tabPanel(
     "The Number of Wildfires in the U.S and the Mean AQI per Year",
     sidebarLayout(
@@ -21,14 +21,25 @@ tab_panel_chart3 <-tabPanel(
         selectInput(
           inputId ="state", label = h4("State"), choices = states, selected = "United States"
         ),
-        h4("Average Air Quality Index and Number of Wildfires per Year"),
+        h4("Number of Wildfires and Average Air Quality Index per Year"),
         
-        p("This is the description of chart 3.")
+        p("This chart shows the relationship between the number of wildfires 
+          in a year and the mean air quality index (AQI) of that year both on a 
+          national and state level. It aims to see how wildfires are affecting
+          air quality in the U.S.. The x-axis of this scatterplot shows the number
+          of wildfires in a year, based off of a random sampling of 50,000 U.S. wildfires.
+          The y-axis is the AQI. Users of the application can select to view this 
+          data for specific states or for the whole of the U.S using the widget above.", 
+          br(), br(), 
+          "Looking at the whole of the U.S, we can see that there is a weak relationship 
+          between the number of wildfires per year and the mean AQI for that year,
+          which is that more wildfires results in a higher AQI (worse air quality).
+          When looking at select states such as ...")
      
          ),
         
         mainPanel(
-          plotlyOutput(OutputId = "chart3")
+          plotlyOutput(outputId = "chart3")
       )
     )
     
