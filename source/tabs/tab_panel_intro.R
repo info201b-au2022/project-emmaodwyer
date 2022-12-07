7b3f3cfd27baa50ce4f581b7b122b6e721eed086
Jennifer <jngomez@uw.edu>
2022-12-07 12:11:47 -0800

    pushing intro page

--- source/tabs/tab_panel_intro.R
+++ source/tabs/tab_panel_intro.R
@@ -3,6 +3,20 @@
 library(shiny)
 
 tab_panel_intro <-tabPanel(
-    "Introduction",
-    p("This is the introduction.")
+  "Introduction",
+  p("With the rise of wildfires in the United States, 
+      air quality has been affected. While short exposure 
+      to air pollution can cause irritation in the eyes, nose, 
+      and throat, long term exposure to air pollutants can cause 
+      more serious damages and diseases such as lung cancer, heart disease, 
+      and strokes. With this project, we aim to analyze the relationships 
+      between the changes in temperature and the environment due to 
+      climate change and how that has affected the number of 
+      wildfires in the United States, and in turn how those wildfires 
+      affected the air quality. More specifically looking into the link 
+      between the wildfires and air quality in the United States per year, 
+      air quality average per year and comparing it to the number of wildfires 
+      per year, and the cause of a wildfire looking at the specific causes and 
+      the proportion of each cause."),
+  img(src='wildfires.jpeg')
 )
